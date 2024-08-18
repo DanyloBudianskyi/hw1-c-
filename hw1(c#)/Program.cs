07d07eaf8f369b0e0f6d3e9b1c06e6f87b58c2ce
Danil <super.danil501@gmail.com>
2024-08-17 22:08:08 -0600

    Complete homework

--- hw1(c#)/Program.cs
+++ hw1(c#)/Program.cs
@@ -10,6 +10,146 @@ namespace hw1_c__
     {
         static void Main(string[] args)
         {
+            //18.Завдання про суму вкладу: Створіть змінні initialDeposit(початковий вклад), annualInterestRate(річна відсоткова ставка) та years(термін вкладу) та обчисліть кінцеву суму вкладу з урахуванням відсотків.
+            //byte years;
+            //Decimal initialDeposit, annualInterestRate;
+            //Console.WriteLine("Input initial deposit: ");
+            //initialDeposit = Decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Input annual Interest Rate(example 0.5): ");
+            //annualInterestRate = Decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Input years: ");
+            //years = Byte.Parse(Console.ReadLine());
+            //Console.WriteLine($"Final amount of deposit after {years} years = {initialDeposit * (decimal)Math.Pow((double)(1 + annualInterestRate / 12),years)}");
+
+
+            //19.Завдання про порівняння знижок: Створіть змінні discount1 та discount2 та порівняйте, яка зі знижок дає велику суму економії.
+            //Console.Write("Input first discount(in percents): ");
+            //decimal discount1 = Decimal.Parse(Console.ReadLine());
+            //Console.Write("Input second discount(in percents): ");
+            //decimal discount2 = Decimal.Parse(Console.ReadLine());
+            //if (discount1 > discount2){ Console.WriteLine("First discount is better"); }
+            //else if (discount1 < discount2) { Console.WriteLine("Second discount is better"); }
+            //else { Console.WriteLine("Discounts are same"); }
+
+            //20.Завдання про витрати на бензин: Створіть змінні fuelPrice (ціна за літр бензину), fuelConsumed (кількість літрів палива) та обчисліть загальні витрати на бензин.
+            //Console.WriteLine("Input fuel price:");
+            //float fuelPrice = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Input how many liters do you need:");
+            //int fuelConsumed = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Total price: {fuelConsumed * fuelPrice}");
+
+            //21.Завдання про довжину кола: Створіть змінну radius(радіус кола) та обчисліть довжину кола.
+            //Console.WriteLine("Input radius: ");
+            //float radius = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"2 * PI * r = {2 * Math.PI * radius} ");
+
+            //22.Завдання про зарплату з бонусами: Створіть змінні baseSalary(базова зарплата) та bonus(сума бонусу) та обчисліть загальну зарплату з урахуванням бонусу.
+            //Console.WriteLine("Input base salary: ");
+            //float baseSalary = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Input bonus");
+            //float bonus = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Salary with bonuses: {baseSalary + bonus}");
+
+            //23.	Завдання про площу прямокутника: Створіть змінні length (довжина) та width (ширина) прямокутника та обчисліть його площу.
+            //Console.WriteLine("Input length:");
+            //float length = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Input width:");
+            //float width = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Area of rectangle: {length * width}");
+
+            //24.	Завдання про витрати на місяць: Створіть змінні monthlyIncome (щомісячний дохід) та expenses (щомісячні витрати) і обчисліть залишок після обліку витрат.
+            //Console.WriteLine("Input monthly income: ");
+            //int monthlyIncome = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Input expenses: ");
+            //int expenses = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine($"Remaining after all expenses: {monthlyIncome - expenses}");
+
+            //25.Завдання про середній бал: Створіть змінні score1, score2, score3, score4 та score5 та обчисліть середній бал за ними.
+            //Console.WriteLine("Input score1:");
+            //byte score1 = Byte.Parse(Console.ReadLine());
+            //Console.WriteLine("Input score2:");
+            //byte score2 = Byte.Parse(Console.ReadLine());
+            //Console.WriteLine("Input score3:");
+            //byte score3 = Byte.Parse(Console.ReadLine());
+            //Console.WriteLine("Input score4:");
+            //byte score4 = Byte.Parse(Console.ReadLine());
+            //Console.WriteLine("Input score5:");
+            //byte score5 = Byte.Parse(Console.ReadLine());
+            //Console.WriteLine($"Average score = {(score1 + score2 + score3 + score4 + score5) / 5}");
+
+            //26.	Завдання про суму чисел: Створіть змінні number1, number2 та number3 та обчисліть їх суму.
+            //Console.WriteLine("Input number1:");
+            //int number1 = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Input number2:");
+            //int number2 = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Input number3:");
+            //int number3 = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine($"{number1 } + {number2} + {number3} = {number1 + number2 + number3}");
+
+            //27.	Завдання про відсоткову ставку: Створіть змінні початковогооб'єму (початкова сума), finalAmount (кінцева сума) і роки (кількість років) і обчисліть річну процентну ставку.
+            //Console.WriteLine("Input initial amount: ");
+            //decimal initialAmount = Decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Input final amount: ");
+            //decimal finalAmount = Decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Input years:");
+            //byte years = Byte.Parse(Console.ReadLine());
+            //Console.WriteLine($"Annual interest rate: {(decimal)Math.Pow((double)(finalAmount / initialAmount), 1.0 / years) - 1}");
+
+            //28.	Завдання про площу кола: Створіть змінну radius (радіус кола) та обчисліть його площу.
+            //Console.WriteLine("Input radius: ");
+            //float radius = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Area of circle = {Math.PI * Math.Pow(radius,2)}");
+
+            //29.	Завдання про кількість сторінок: Створіть змінні totalPages (загальна кількість сторінок) та pagesRead (кількість прочитаних сторінок) та визначте, скільки сторінок залишилося прочитати.
+            //Console.WriteLine("Input total pages:");
+            //int totalPages = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Input pages read:");
+            //int pagesRead = Int32.Parse(Console.ReadLine());
+            //if (totalPages > pagesRead) { Console.WriteLine($"You need to read - {totalPages - pagesRead} pages"); }
+            //else { Console.WriteLine("pagesRead > totalPages"); }
+
+            //30.	Завдання про ціну квитків: Створіть змінні adultTickets (кількість квитків для дорослих) та childTickets (кількість квитків для дітей) та обчисліть загальну вартість квитків на захід.
+            //int adultTicketPrice = 100;
+            //int childTicketsPrice = 60;
+            //Console.WriteLine("How many adult tickets do you need?:");
+            //int adultTickets = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("How many child tickets do you need?:");
+            //int childTickets = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine($"Total price: {(adultTicketPrice * adultTickets) + (childTicketsPrice * childTickets)}");
+
+            //31.	Завдання про витрати на навчання: Створіть змінні tuitionFee (вартість навчання), booksCost (вартість підручників) та livingExpenses (витрати на проживання) і обчисліть загальні витрати на навчання.
+            //Console.WriteLine("Input tuition fee:");
+            //int tuitionFee = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Input books price:");
+            //int booksCost = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Input living expenses:");
+            //int livingExpenses = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine($"Total fee: {tuitionFee + booksCost + livingExpenses}");
+
+            //32.	Завдання про кількість слів: Створіть змінну текст (текстовий рядок) та обчисліть кількість слів у тексті.
+            Console.WriteLine("Input text:");
+            string text = Console.ReadLine();
+            string[] words = text.Split(' ');
+            Console.WriteLine(words.Length);
+
+
+            //33.Завдання про місткість сховища: Створіть змінні totalCapacity(загальна місткість сховища) і usedCapacity(використовується місткість) і визначте, скільки вільного місця залишилося.
+            //Console.WriteLine("Input total capacity:");
+            //int totalCapacity = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Input used capacity:");
+            //int usedCapacity = Int32.Parse(Console.ReadLine());
+            //if (usedCapacity < totalCapacity) { Console.WriteLine($"Free place in capacity = {totalCapacity - usedCapacity}"); }
+            //else { Console.WriteLine("Used capacity <= total capacity"); }
+
+            //34.	Завдання про зарплату після утримання: Створіть змінні grossSalary (грос-зарплата) та taxRate (ставка податку) та обчисліть чисту зарплату після утримань.
+            //Console.WriteLine("Input gross salary:");
+            //int grossSalary = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Input tax rate(in percents):");
+            //float taxRate = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Salary = {grossSalary - (grossSalary * (taxRate/100))}");
+
+
+            Console.ReadKey();
         }
     }
 }
